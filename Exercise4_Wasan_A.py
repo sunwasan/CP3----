034c800333e5c6dd2b099f68b095a43650ec5a5b
sunwasan <105874427+sunwasan@users.noreply.github.com>
2022-05-20 12:37:08 +0700

    Summary

---
+++ Exercise4_Wasan_A.py
@@ -0,0 +1,9 @@
+FE = 73.00
+GB = 54.35
+ICS = 93.81
+CP = 87.63
+print("---Your Score---")
+print("Foudation English:",FE)
+print("General Business:",GB)
+print("Introduction to Computer Systems:",ICS)
+print("Computer Programming:",CP)
