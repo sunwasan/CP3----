a1c9c2fa321b203fa8fd115d407483c691f2d3d6
sunwasan <105874427+sunwasan@users.noreply.github.com>
2022-05-21 12:03:46 +0700

    Create Lecture50_Wasan_A.py

---
+++ Lecture50_Wasan_A.py
@@ -0,0 +1,12 @@
+def plus(x,y):
+    print(x+y)
+def minus(x,y):
+    print(x*y)
+def time(x,y):
+    print(x*y)
+def divide(x,y):
+    print(x/y)
+plus(10,20)
+minus(10,20)
+time(10,20)
+divide(10,20)
